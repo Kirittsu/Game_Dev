1774065c804dc590a87e07cff71a62d6f5426379
keanu segers <keanusegers@hotmail.com>
2024-01-03 23:39:45 +0100

    Minor add in Lvl1

--- Game_Dev/Screens/Level1.cs
+++ Game_Dev/Screens/Level1.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Game_Dev.Charaters;
+using Game_Dev.Characters;
 using Microsoft.Xna.Framework;
 
 namespace Game_Dev.Screens
