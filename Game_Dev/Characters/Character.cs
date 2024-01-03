00620155894bc36187e25eb5637135bf625bd6d3
keanu segers <keanusegers@hotmail.com>
2024-01-03 23:41:04 +0100

    Fix typo and Add Animate interface

---
+++ Game_Dev/Characters/Character.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Game_Dev.Managers;
+using Game_Dev.Objects;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Game_Dev.Characters
+{
+    internal abstract class Character : BaseObject
+    {
+        
+    }
+}
