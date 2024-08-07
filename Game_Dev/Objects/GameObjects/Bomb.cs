2cb7e46b76c9404d6661f52144ae9bf25af07b25
Kirittsu <keanusegers@hotmail.com>
2024-08-07 22:09:39 +0200

    Add bomb

---
+++ Game_Dev/Objects/GameObjects/Bomb.cs
@@ -0,0 +1,43 @@
+﻿using Game_Dev.Characters;
+using Game_Dev.Managers;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game_Dev.Objects.GameObjects
+{
+    public class Bomb : Character
+    {
+        public Character Origin;
+
+        public Bomb(Vector2 position, Vector2 facing, Character origin)
+        {
+            Texture = GameStateManager.content.Load<Texture2D>("bomb");
+            MinPosition = position;
+            Origin = origin;
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+        }
+
+        public override bool Interaction(BaseObject gameObject)
+        {
+            if (gameObject == Origin || gameObject is Hero)
+            {
+                if (gameObject is Hero)
+                {
+                    GameStateManager.Remove(this);
+                }
+                return false;
+            }
+            GameStateManager.Remove(this);
+            return true;
+        }
+    }
+}
